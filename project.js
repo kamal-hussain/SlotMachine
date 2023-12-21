02ad8dd5e334cf193362421fed62af5ee077a0c1
Kamal Hussain <75313243+kamal-hussain@users.noreply.github.com>
2023-12-21 11:39:39 +0000

    Deposit amounts and Number of lines outputs in the console.log
    Deposit amounts and Number of lines outputs in the console.log

---
+++ project.js
@@ -0,0 +1,41 @@
+//1. Deposit some money
+//2. Determine number if lines to bet on
+//3. Collect the bet
+
+const prompt = require("prompt-sync")();
+
+const deposit = () => {
+  while (true) {
+    const depositAmount = prompt("Enter a deposit amount:");
+    const numberDepositAmount = parseFloat(depositAmount);
+
+    if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+      console.log("Please enter a valid number");
+    } else {
+      return numberDepositAmount;
+    }
+  }
+};
+
+const getNumberOfLines = () => {
+  while (true) {
+    const lines = prompt("Enter the number of lines to bet on (1-3): ");
+    const numberOfLines = parseFloat(lines);
+
+    if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+      console.log("Invalid number of lines, try again");
+    } else {
+      return numberOfLines;
+    }
+  }
+};
+
+const depositAmount = deposit();
+const numberOfLines = getNumberOfLines();
+
+console.log(
+  "Deposit Amount:",
+  depositAmount,
+  "Number of lines:",
+  numberOfLines
+);
